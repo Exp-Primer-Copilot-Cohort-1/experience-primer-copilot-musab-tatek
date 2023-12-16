c38d461c303649fdfc96e21217a1859315e31459
musab-tatek <101246614+musab-tatek@users.noreply.github.com>
2023-12-16 07:04:30 +0300

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// src/app/app.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Welcome to My Angular Web Server!</h1>',
+  styles: []
+})
+export class AppComponent {}
