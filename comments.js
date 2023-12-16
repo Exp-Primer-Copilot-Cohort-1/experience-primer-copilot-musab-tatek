8f3bac68c575e86436a1476774fd306a7bfd5238
musab-tatek <101246614+musab-tatek@users.noreply.github.com>
2023-12-16 07:06:57 +0300

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,20 @@
-// src/app/app.component.ts
+// Create web server
+import { Component, enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { Component } from '@angular/core';
-
+// Define the main Angular component
 @Component({
   selector: 'app-root',
   template: '<h1>Welcome to My Angular Web Server!</h1>',
   styles: []
 })
 export class AppComponent {}
+
+// Enable production mode if applicable
+if (environment.production) {
+  enableProdMode();
+}
+
+// Bootstrap the Angular module
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
