11b7a6edccd1239e82941d529e53d153938442c6
musab-tatek <101246614+musab-tatek@users.noreply.github.com>
2023-12-16 07:00:37 +0300

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,12 @@
+function calculateNumbers(var1, var2) {
+    // Perform some calculation here
+    // For example, let's add var1 and var2
+    let result = var1 + var2;
+  
+    // Return the result
+    return result;
+  }
+  
+  // Example usage:
+  let resultOfCalculation = calculateNumbers(5, 10);
+  console.log(resultOfCalculation); // Output: 15  
